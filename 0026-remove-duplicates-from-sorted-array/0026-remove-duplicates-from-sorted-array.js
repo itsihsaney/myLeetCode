12e60c5b0a78127bf63d7331869d15b51c03d52a
Mohammed Ihsan <muhammedihsan0010@gmail.com>
2026-02-24 09:30:30 +0530

    Time: 58 ms (5.47%), Space: 58 MB (57.57%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let numsLength = nums.length;
+    for (let i = 0; i < numsLength; i++) {
+        if (i >= nums.length) {
+            break;
+        }
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+};
